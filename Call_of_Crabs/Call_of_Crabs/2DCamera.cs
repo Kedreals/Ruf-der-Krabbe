d1da997c4294e6e37ac8b12268b680bd45ef359c
KEDREALS\ebert <ebert.mueden1@freenet.de>
2017-10-20 23:03:03 +0200

    push

--- Call_of_Crabs/Call_of_Crabs/2DCamera.cs
+++ Call_of_Crabs/Call_of_Crabs/2DCamera.cs
@@ -37,5 +37,15 @@ namespace Call_of_Crabs
                                          Matrix.CreateScale(new Vector3(m_scale, m_scale, 1)) *
                                          Matrix.CreateTranslation(new Vector3(m_device.Viewport.Width * 0.5f, m_device.Viewport.Height * 0.5f, 0));
         }
+
+        public void Test(SpriteBatch batch)
+        {
+            Rectangle r = new Rectangle((int)(1.0f/m_scale * (-m_device.PresentationParameters.BackBufferWidth / 2)), (int)(1.0f/m_scale * (-m_device.PresentationParameters.BackBufferHeight / 2)), 
+                (int)(1.0f/m_scale *( m_device.PresentationParameters.BackBufferWidth /* 2*/)), (int)(1.0f/m_scale * (m_device.PresentationParameters.BackBufferHeight /* 2*/)));
+            Texture2D t = new Texture2D(m_device, 1, 1);
+            t.SetData(new Color[] { Color.Black });
+
+            batch.Draw(t, r, Color.White);
+        }
     }
 }
