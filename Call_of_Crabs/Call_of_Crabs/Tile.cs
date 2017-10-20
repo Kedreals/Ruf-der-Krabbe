b192c48cf77fc8e63aa3ebe8532e5c9a301f54ac
Lisa <li.pi.97@gmx.de>
2017-10-20 23:00:59 +0200

    Controls and Start tiles

---
+++ Call_of_Crabs/Call_of_Crabs/Tile.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace Call_of_Crabs
+{
+    public enum TileType
+    {
+        InnerTile,
+        SurfaceTile
+    }
+
+    public class Tile
+    {
+        private Texture2D texture;
+
+        private Vector2 position;
+        public Vector2 Position
+        {
+            get { return position; }
+            set
+            {
+                position = value;
+                this.TileRectangle.Location = position.ToPoint();
+            } }
+        public Vector2 Size;
+        public Rectangle TileRectangle;
+        public TileType Type;
+        public bool IsHostile;
+
+        public Tile(TileType type, Vector2 position, Vector2 size = default(Vector2), bool isHostile = false)
+        {
+            Type = type;
+            Position = position;
+            if (size.Length() > 0)
+            {
+                Size = size;
+            }
+            else
+            {
+                Size = new Vector2(20, 20);
+            }
+            IsHostile = isHostile;
+
+            TileRectangle = new Rectangle(position.ToPoint(), Size.ToPoint());
+        }
+
+        public void Load(ContentManager contentManager, string filename)
+        {
+            texture = contentManager.Load<Texture2D>("Textures/" + filename);
+        }
+
+        public void Move(Vector2 vec)
+        {
+            Position += vec;
+            TileRectangle.Location = Position.ToPoint();
+        }
+
+        public void Scale(Vector2 vec)
+        {
+            Size *= vec;
+            TileRectangle.Size = Size.ToPoint();
+        }
+    }
+}
