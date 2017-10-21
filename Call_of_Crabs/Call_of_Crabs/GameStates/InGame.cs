14e44bfce9b3cf5546ca2431121b74e85814d46e
COOKIE\Robert <robert.mueller@st.ovgu.de>
2017-10-21 14:06:49 +0200

    added kritzler as character

--- Call_of_Crabs/Call_of_Crabs/GameStates/InGame.cs
+++ Call_of_Crabs/Call_of_Crabs/GameStates/InGame.cs
@@ -14,27 +14,31 @@ namespace Call_of_Crabs.GameStates
     {
         private Map map = new Map();
         private Player player = new Player(new Rectangle(25, 25, 100, 50), new Rectangle(0, 0, 200, 100));
+        private KritzlerEnemy kritzler = new KritzlerEnemy(new Rectangle(15, 15, 70, 70), new Rectangle(0, 0, 100, 100));
         Camera2D camera;
-        Texture2D test;
 
         public void Initialize(GraphicsDevice graphics)
         {
             camera = new Camera2D(graphics);
             camera.Position += new Vector2(-70, -150);
+            kritzler.Position += new Vector2(70, 150);
         }
 
         public void LoadContent(ContentManager contentManager)
         {
-            test = contentManager.Load<Texture2D>("Textures/Kitzler");
             map.Load(contentManager, "TestMap");
             player.Load(contentManager,"");
+            kritzler.Load(contentManager, "");
         }
 
         public EGameState Update(GameTime time)
         {
             player.Update(time);
+            kritzler.Update(time);
+
 
             player.Collide(map);
+            kritzler.Collide(map);
 
             camera.Position = player.sprite.Center.ToVector2();
 
@@ -46,7 +50,7 @@ namespace Call_of_Crabs.GameStates
             batch.Begin(SpriteSortMode.Deferred, null, null, null, null, null, camera.GetTransform());
 
             map.Draw(batch);
-            batch.Draw(test, Vector2.Zero);
+            kritzler.Draw(batch);
             player.Draw(batch);
 
             batch.End();
